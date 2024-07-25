d6151466d5ea86f109d4d24d59f5d98fefa722a3
hamze <hamze.mostafaei@gmail.com>
2024-07-25 18:25:51 +0330

    Modules refined

---
+++ application/src/main/java/com/hamze/banking/system/application/BankingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hamze.banking.system.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.hamze.banking.system")
+public class BankingSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BankingSystemApplication.class, args);
+    }
+}
