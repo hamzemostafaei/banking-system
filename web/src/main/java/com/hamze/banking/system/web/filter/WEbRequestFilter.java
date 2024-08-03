72d162b7ce338392d2b5279747d6cd043e13a268
hamze <hamze.mostafaei@gmail.com>
2024-08-03 16:18:24 +0330

    Tracking number added to MDC

--- web/src/main/java/com/hamze/banking/system/web/filter/WEbRequestFilter.java
+++ web/src/main/java/com/hamze/banking/system/web/filter/WEbRequestFilter.java
@@ -1,11 +1,12 @@
 package com.hamze.banking.system.web.filter;
 
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hamze.banking.system.shared.util.SnowFlakeUniqueIDGenerator;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.Ordered;
@@ -14,6 +15,8 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
 
 @Slf4j
 @Component
@@ -21,9 +24,11 @@ import java.util.Date;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class WEbRequestFilter implements Filter {
 
-    public static final String REQUEST_ID_CONTEXT_KEY = "RQST-ID";
+    public static final String TRACKING_NUMBER_CONTEXT_KEY = "TRACKING-NUMBER";
+    public static final String TRANSACTION_ID_CONTEXT_KEY = "TRANSACTION_ID";
     public static final String REQUEST_DATE_CONTEXT_KEY = "RQST-DATE";
     public static final String NODE_ID_CONTEXT_KEY = "NODE-ID";
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Value("${com.hamze.banking.system.node-id}")
     private Long nodeId;
@@ -31,21 +36,36 @@ public class WEbRequestFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
+        CachedBodyHttpServletRequest cachedRequest = new CachedBodyHttpServletRequest((HttpServletRequest) request);
+
         try {
-            String requestId = Long.toString(SnowFlakeUniqueIDGenerator.generateNextId(nodeId));
+            String trackingNumber = UUID.randomUUID().toString();
             String transactionId = Long.toString(SnowFlakeUniqueIDGenerator.generateNextId(nodeId));
             Date requestDate = new Date();
+
             if (request instanceof HttpServletRequest httpRequest) {
-                Object trackingNumber = httpRequest.getAttribute("trackingNumber");
-                if (trackingNumber == null) {
-                    httpRequest.setAttribute("trackingNumber", requestId);
+                String requestBody = new String(cachedRequest.getInputStream().readAllBytes());
+
+                Object requestTrackingNumber = null;
+                if (StringUtils.isNoneBlank(requestBody)) {
+                    @SuppressWarnings("unchecked")
+                    Map<String, Object> bodyMap = objectMapper.readValue(requestBody, Map.class);
+                    requestTrackingNumber = bodyMap.get("trackingNumber");
+                }
+
+                if (requestTrackingNumber == null) {
+                    httpRequest.setAttribute("trackingNumber", trackingNumber);
+                } else {
+                    trackingNumber = requestTrackingNumber.toString();
                 }
                 httpRequest.setAttribute("registrationDate", requestDate);
                 httpRequest.setAttribute("transactionId", transactionId);
             }
+
             MDC.put(NODE_ID_CONTEXT_KEY, String.valueOf(nodeId));
-            MDC.put(REQUEST_ID_CONTEXT_KEY, requestId);
+            MDC.put(TRACKING_NUMBER_CONTEXT_KEY, trackingNumber);
             MDC.put(REQUEST_DATE_CONTEXT_KEY, Long.toString(requestDate.getTime()));
+            MDC.put(TRANSACTION_ID_CONTEXT_KEY, transactionId);
 
         } catch (Exception ex) {
             if (log.isWarnEnabled()) {
@@ -54,6 +74,6 @@ public class WEbRequestFilter implements Filter {
             }
         }
 
-        chain.doFilter(request, response);
+        chain.doFilter(cachedRequest, response);
     }
 }
